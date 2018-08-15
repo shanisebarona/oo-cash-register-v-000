dcc78c64a26f25049f92244fc990d0819303e407
Shanise Barona <shanisebarona@gmail.com>
2018-08-15 23:14:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require 'pry'
 
+class CashRegister
+  
+attr_accessor :discount, :total
+
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+  end
+  
+  def apply_discount 
+    @total / @discount.to_f
+    binding.pry
+  end
+  
+end
