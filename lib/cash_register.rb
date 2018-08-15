d58079cf25daa65b4cb4b832a66dee456f362d6d
Shanise Barona <shanisebarona@gmail.com>
2018-08-15 23:48:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ attr_accessor :discount, :total
   def add_item(title, price, quantity = 1)
     @total += price * quantity
     @items.fill(title, @items.size, quantity)
-    binding.pry
+    # binding.pry
   end
   
   def apply_discount 
